22d5234cf14a0863f3206d447ecbae1aa53d1109
Alexander Sohn <github@asohn.de>
2022-10-30 12:27:13 +0100

    Add validation for paper

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,7 @@
 class Paper < ApplicationRecord
+    validates :title, presence: true,
+                    length: { minimum: 1 }
+    validates :venue, presence: true,
+                    length: { minimum: 1 }
+    validates :year, presence: true
 end
